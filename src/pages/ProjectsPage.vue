2a7417fca87677ef360aed6ee355a22127c29c26
DrKillex <lcollaro1994@gmail.com>
2023-05-29 20:09:48 +0200

    feat paginate

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -8,6 +8,8 @@ export default {
     data() {
         return {
             store,
+            showButton: true,
+            currentPage:1,
         }
     },
     components: {
@@ -15,14 +17,27 @@ export default {
     },
     methods: {
         getRecords() {
-            axios.get(this.store.apiBaseUrl + this.store.apiUrls.records)
+            axios.get(this.store.apiBaseUrl + this.store.apiUrls.records, {
+                params: {
+                    page: this.currentPage
+                }
+            })
                 .then((response) => {
-                    this.store.records = response.data.results;
+                    const results = response.data.results.data ?? response.data.results;
+                    const isNextAvaiable = response.data.results.next_page_url ?? null;
+                    this.store.records = [...this.store.records, ...results];
                     console.log(this.store.records)
+                    if (!isNextAvaiable) {
+                        this.showButton = false;
+                    }
                 })
                 .catch((error) => {
                     console.log(error);
                 })
+        },
+        nextPage() {
+            this.currentPage += 1;
+            this.getRecords();
         }
     },
     created() {
@@ -38,6 +53,9 @@ export default {
                 <div class="row my-4 gy-4">
                     <div class="col col-md-4" v-for="record in store.records">
                         <ProjectCard :record="record" />
+                    </div>
+                    <div class="text-center my-5" v-if="showButton">
+                        <button class="btn btn-primary" @click.prevent="nextPage">More</button>
                     </div>
                 </div>
             </div>
